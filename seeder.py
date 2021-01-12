7036e55c992f8a1d576e4c44e24a6561465b31db
Ram <rkm1415@gmail.com>
2021-01-12 12:51:36 +0530

    seed model

--- seeder.py
+++ seeder.py
@@ -9,34 +9,31 @@ django.setup()
 from lms.models.dummy_model import DummyStudent, DummyAssignment
 from django_seed import Seed
 
-
-
-# assert num_students_to_add < number_per_assignment, "Can't have more than one assignment per student"
-    
-
 seeder = Seed.seeder()
 
 num_per_assignment = 10
 mark_base = 100
 num_students = 10
+num_assignments = 100
+max_mark = 1000
 
+seeder.add_entity(DummyStudent, num_students, {
+    "email": lambda x:seeder.faker.email(),
+    "name": lambda x:seeder.faker.name(),
+})
+inserted_pks = seeder.execute()
 
-
-# seeder.add_entity(DummyStudent, num_students, {
-#     "email": lambda x:seeder.faker.email(),
-#     "name": lambda x:seeder.faker.name(),
-# })
-# inserted_pks = seeder.execute()
-# print(inserted_pks)
-
-for student in range(100):
+for student in DummyStudent.objects.all():
     for n in range(1, num_per_assignment):
-        seeder.add_entity(DummyAssignment, 1, {
-            # "student":student,
-            "title": f"Assignment {n}",
+        title = f"Assignment {n}"
+        post = {
+            "student":student,
+            "title": title,
             "max_mark": mark_base*n,
-            "alloted_mark": lambda x: random.randint(0, mark_base*n),
-            "grade": lambda x: random.choice('ABCDEF'),
+            "alloted_mark": random.randint(0, mark_base*n),
+            "grade": random.choice('ABCDEF'),
 
-        })
-        inserted_pks = seeder.execute()
+        }
+        as1  = DummyAssignment(**post)
+        as1.save()
+
